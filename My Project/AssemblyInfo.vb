87a9ba008db614cab8c68712d56ce28715ab3aef
Christopher Gedler <45577870+cgedler@users.noreply.github.com>
2021-03-07 12:13:19 -0400

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Guia Fletes GAG")> 
+<Assembly: AssemblyDescription("Módulo para generar las Guías")> 
+<Assembly: AssemblyCompany("Fletes GAG, C.A.")> 
+<Assembly: AssemblyProduct("GuiaFletesGAG")> 
+<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyTrademark("Fletes GAG")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("3a07f988-3813-4ed6-8d65-f4ea16660a71")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de versión de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
